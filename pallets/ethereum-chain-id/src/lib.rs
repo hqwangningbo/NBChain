d4f4401c6462fe8f30da93a04417cad0c74b7da1
NingBo Wang <2536935847@qq.com>
2021-12-09 11:57:09 +0800

    delete unused import

--- pallets/ethereum-chain-id/src/lib.rs
+++ pallets/ethereum-chain-id/src/lib.rs
@@ -1,39 +1,39 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+
 pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
-	use frame_system::pallet_prelude::*;
+    use frame_support::pallet_prelude::*;
 
-	#[pallet::config]
-	pub trait Config: frame_system::Config {}
+    #[pallet::config]
+    pub trait Config: frame_system::Config {}
 
-	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
+    #[pallet::pallet]
+    #[pallet::generate_store(pub (super) trait Store)]
+    pub struct Pallet<T>(_);
 
-	#[pallet::storage]
-	#[pallet::getter(fn chain_id)]
-	pub type ChainId<T> = StorageValue<_, u64, ValueQuery>;
+    #[pallet::storage]
+    #[pallet::getter(fn chain_id)]
+    pub type ChainId<T> = StorageValue<_, u64, ValueQuery>;
 
-	//小心这个没有范形
-	#[pallet::genesis_config]
-	pub struct GenesisConfig {
-		pub chain_id: u64,
-	}
+    //小心这个没有范形
+    #[pallet::genesis_config]
+    pub struct GenesisConfig {
+        pub chain_id: u64,
+    }
 
-	#[cfg(feature = "std")]
-	impl Default for GenesisConfig {
-		fn default() -> Self {
-			Self { chain_id: 1500u64 }
-		}
-	}
+    #[cfg(feature = "std")]
+    impl Default for GenesisConfig {
+        fn default() -> Self {
+            Self { chain_id: 1500u64 }
+        }
+    }
 
-	#[pallet::genesis_build]
-	impl<T: Config> GenesisBuild<T> for GenesisConfig {
-		fn build(&self) {
-			ChainId::<T>::put(self.chain_id);
-		}
-	}
+    #[pallet::genesis_build]
+    impl<T: Config> GenesisBuild<T> for GenesisConfig {
+        fn build(&self) {
+            ChainId::<T>::put(self.chain_id);
+        }
+    }
 }
