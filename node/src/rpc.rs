7236bcade128995690a4c1306cb8336e8489aed2
NingBo Wang <2536935847@qq.com>
2022-01-26 00:44:16 +0800

    Add eth rpc

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -7,48 +7,185 @@
 
 use std::sync::Arc;
 
-use nbchain_runtime::{opaque::Block, AccountId, Balance, Index};
+use nbchain_runtime::{opaque::Block, AccountId, Balance, Index, Hash};
+use sc_network::NetworkService;
 pub use sc_rpc_api::DenyUnsafe;
 use sc_transaction_pool_api::TransactionPool;
 use sp_api::ProvideRuntimeApi;
 use sp_block_builder::BlockBuilder;
 use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
 
+// EVM
+use std::collections::BTreeMap;
+use sp_runtime::traits::BlakeTwo256;
+use sc_client_api::{
+    backend::{AuxStore, Backend, StateBackend, StorageProvider},
+    client::BlockchainEvents,
+};
+use sc_transaction_pool::{ChainApi, Pool};
+use fc_rpc_core::types::{FeeHistoryCache, FilterPool};
+use jsonrpc_pubsub::manager::SubscriptionManager;
+use pallet_ethereum::EthereumStorageSchema;
+use fc_rpc::{
+    EthBlockDataCache, OverrideHandle, RuntimeApiStorageOverride, SchemaV1Override,
+    SchemaV2Override, StorageOverride,
+};
+use sc_rpc::SubscriptionTaskExecutor;
+
 /// Full client dependencies.
-pub struct FullDeps<C, P> {
-	/// The client instance to use.
-	pub client: Arc<C>,
-	/// Transaction pool instance.
-	pub pool: Arc<P>,
-	/// Whether to deny unsafe calls
-	pub deny_unsafe: DenyUnsafe,
+pub struct FullDeps<C, P, A: ChainApi> {
+    /// The client instance to use.
+    pub client: Arc<C>,
+    /// Transaction pool instance.
+    pub pool: Arc<P>,
+    /// Graph pool instance.
+    pub graph: Arc<Pool<A>>,
+    /// Whether to deny unsafe calls
+    pub deny_unsafe: DenyUnsafe,
+    /// The Node authority flag
+    pub is_authority: bool,
+    /// Network service
+    pub network: Arc<NetworkService<Block, Hash>>,
+    /// EthFilterApi pool.
+    pub filter_pool: Option<FilterPool>,
+    /// Backend.
+    pub backend: Arc<fc_db::Backend<Block>>,
+    /// Maximum number of logs in a query.
+    pub max_past_logs: u32,
+    /// Maximum fee history cache size.
+    pub fee_history_limit: u64,
+    /// Fee history cache.
+    pub fee_history_cache: FeeHistoryCache,
+    /// Cache for Ethereum block data.
+    pub block_data_cache: Arc<EthBlockDataCache<Block>>,
+}
+
+pub fn overrides_handle<C, BE>(client: Arc<C>) -> Arc<OverrideHandle<Block>>
+    where
+        C: ProvideRuntimeApi<Block> + StorageProvider<Block, BE> + AuxStore,
+        C: HeaderBackend<Block> + HeaderMetadata<Block, Error=BlockChainError>,
+        C: Send + Sync + 'static,
+        C::Api: fp_rpc::EthereumRuntimeRPCApi<Block>,
+        BE: Backend<Block> + 'static,
+        BE::State: StateBackend<BlakeTwo256>,
+{
+    let mut overrides_map = BTreeMap::new();
+    overrides_map.insert(
+        EthereumStorageSchema::V1,
+        Box::new(SchemaV1Override::new(client.clone()))
+            as Box<dyn StorageOverride<_> + Send + Sync>,
+    );
+    overrides_map.insert(
+        EthereumStorageSchema::V2,
+        Box::new(SchemaV2Override::new(client.clone()))
+            as Box<dyn StorageOverride<_> + Send + Sync>,
+    );
+
+    Arc::new(OverrideHandle {
+        schemas: overrides_map,
+        fallback: Box::new(RuntimeApiStorageOverride::new(client)),
+    })
 }
 
 /// Instantiate all full RPC extensions.
-pub fn create_full<C, P>(deps: FullDeps<C, P>) -> jsonrpc_core::IoHandler<sc_rpc::Metadata>
-where
-	C: ProvideRuntimeApi<Block>,
-	C: HeaderBackend<Block> + HeaderMetadata<Block, Error = BlockChainError> + 'static,
-	C: Send + Sync + 'static,
-	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Index>,
-	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
-	C::Api: BlockBuilder<Block>,
-	P: TransactionPool + 'static,
+pub fn create_full<C, P, BE, A>(
+    deps: FullDeps<C, P, A>,
+    subscription_task_executor: SubscriptionTaskExecutor,
+    overrides: Arc<OverrideHandle<Block>>) -> jsonrpc_core::IoHandler<sc_rpc::Metadata>
+    where
+        BE: Backend<Block> + 'static,
+        BE::State: StateBackend<BlakeTwo256>,
+        C: ProvideRuntimeApi<Block> + StorageProvider<Block, BE> + AuxStore,
+        C: BlockchainEvents<Block>,
+        C: HeaderBackend<Block> + HeaderMetadata<Block, Error=BlockChainError>,
+        C: Send + Sync + 'static,
+        C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Index>,
+        C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
+        C::Api: BlockBuilder<Block>,
+        C::Api: fp_rpc::EthereumRuntimeRPCApi<Block>,
+        P: TransactionPool<Block=Block> + Sync + Send + 'static,
+        A: ChainApi<Block=Block> + 'static,
 {
-	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApi};
-	use substrate_frame_rpc_system::{FullSystem, SystemApi};
+    use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApi};
+    use substrate_frame_rpc_system::{FullSystem, SystemApi};
+    use fc_rpc::{
+        EthApi, EthApiServer, EthDevSigner, EthFilterApi, EthFilterApiServer, EthPubSubApi,
+        EthPubSubApiServer, EthSigner, HexEncodedIdProvider, NetApi, NetApiServer, Web3Api,
+        Web3ApiServer,
+    };
 
-	let mut io = jsonrpc_core::IoHandler::default();
-	let FullDeps { client, pool, deny_unsafe } = deps;
+    let mut io = jsonrpc_core::IoHandler::default();
+    let FullDeps {
+        client,
+        pool,
+        graph,
+        deny_unsafe,
+        is_authority,
+        network,
+        filter_pool,
+        backend,
+        max_past_logs,
+        fee_history_limit,
+        fee_history_cache,
+        block_data_cache,
+    } = deps;
 
-	io.extend_with(SystemApi::to_delegate(FullSystem::new(client.clone(), pool, deny_unsafe)));
 
-	io.extend_with(TransactionPaymentApi::to_delegate(TransactionPayment::new(client.clone())));
+    io.extend_with(SystemApi::to_delegate(FullSystem::new(client.clone(), pool.clone(), deny_unsafe)));
 
-	// Extend this RPC with a custom API by using the following syntax.
-	// `YourRpcStruct` should have a reference to a client, which is needed
-	// to call into the runtime.
-	// `io.extend_with(YourRpcTrait::to_delegate(YourRpcStruct::new(ReferenceToClient, ...)));`
+    io.extend_with(TransactionPaymentApi::to_delegate(TransactionPayment::new(client.clone())));
 
-	io
+    // Extend this RPC with a custom API by using the following syntax.
+    // `YourRpcStruct` should have a reference to a client, which is needed
+    // to call into the runtime.
+    // `io.extend_with(YourRpcTrait::to_delegate(YourRpcStruct::new(ReferenceToClient, ...)));`
+    {
+        io.extend_with(EthApiServer::to_delegate(EthApi::new(
+            client.clone(),
+            pool.clone(),
+            graph,
+            nbchain_runtime::TransactionConverter,
+            network.clone(),
+            Vec::new(),
+            overrides.clone(),
+            backend.clone(),
+            is_authority,
+            max_past_logs,
+            block_data_cache.clone(),
+            fee_history_limit,
+            fee_history_cache,
+        )));
+
+        if let Some(filter_pool) = filter_pool {
+            io.extend_with(EthFilterApiServer::to_delegate(EthFilterApi::new(
+                client.clone(),
+                backend,
+                filter_pool,
+                500_usize, // max stored filters
+                max_past_logs,
+                block_data_cache.clone(),
+            )));
+        }
+
+        io.extend_with(NetApiServer::to_delegate(NetApi::new(
+            client.clone(),
+            network.clone(),
+            // Whether to format the `peer_count` response as Hex (default) or not.
+            true,
+        )));
+
+        io.extend_with(Web3ApiServer::to_delegate(Web3Api::new(client.clone())));
+
+        io.extend_with(EthPubSubApiServer::to_delegate(EthPubSubApi::new(
+            pool,
+            client.clone(),
+            network,
+            SubscriptionManager::<HexEncodedIdProvider>::with_id_provider(
+                HexEncodedIdProvider::default(),
+                Arc::new(subscription_task_executor),
+            ),
+            overrides,
+        )));
+    }
+    io
 }
