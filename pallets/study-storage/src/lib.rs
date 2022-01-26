13f37918440317a22c5e9e5f6371b1d6ca9450a1
NingBo Wang <2536935847@qq.com>
2022-01-26 18:43:58 +0800

    Update study-storage

--- pallets/study-storage/src/lib.rs
+++ pallets/study-storage/src/lib.rs
@@ -1,45 +1,89 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use pallet::*;
+use frame_support::pallet_prelude::*;
+use sp_std::vec::Vec;
+
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Eq, PartialEq, Encode, Decode, scale_info::TypeInfo)]
+#[cfg_attr(feature = "std", derive(Debug, Serialize, Deserialize))]
+#[cfg_attr(feature = "std", serde(rename_all = "camelCase"))]
+pub struct Person {
+    pub name: Vec<u8>,
+    pub age: u8,
+    pub address: Vec<u8>,
+}
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
-	use frame_system::pallet_prelude::*;
+    use super::*;
+    use frame_support::dispatch::DispatchResult;
+    use frame_system::pallet_prelude::*;
 
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-	}
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
 
-	#[pallet::pallet]
-	#[pallet::generate_store(pub (super) trait Store)]
-	pub struct Pallet<T>(_);
+    #[pallet::pallet]
+    #[pallet::generate_store(pub (super) trait Store)]
+    pub struct Pallet<T>(_);
 
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	pub type Something<T> = StorageValue<_, u32>;
+    #[pallet::storage]
+    #[pallet::getter(fn person_id)]
+    pub type PersonId<T> = StorageValue<_, u32, ValueQuery>;
 
-	#[pallet::event]
-	#[pallet::generate_deposit(pub (super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		SomethingStored(u32, T::AccountId),
-	}
+    #[pallet::storage]
+    #[pallet::getter(fn get_person)]
+    pub type Persons<T: Config> = StorageMap<_, Blake2_128Concat, u32, Person>;
 
-	#[pallet::error]
-	pub enum Error<T> {
-		NoneValue,
-		StorageOverflow,
-	}
+    #[pallet::event]
+    #[pallet::generate_deposit(pub (super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        AddPerson(T::AccountId, u32, Person),
+        RemovePerson(T::AccountId, u32, Person),
+        DeletePerson(T::AccountId, u32, Person),
+        UpdatePerson(T::AccountId, u32, Person),
+    }
 
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			let who = ensure_signed(origin)?;
-			<Something<T>>::put(something);
-			Self::deposit_event(Event::SomethingStored(something, who));
-			Ok(())
-		}
-	}
+    #[pallet::error]
+    pub enum Error<T> {
+        NoneValue,
+        PersonIdOverflow,
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(10_000)]
+        pub fn add_person(origin: OriginFor<T>, name: Vec<u8>, age: u8, address: Vec<u8>) -> DispatchResult {
+            let caller = ensure_signed(origin)?;
+            let person_id = PersonId::<T>::get();
+            let person = Person {
+                name,
+                age,
+                address,
+            };
+            Persons::<T>::insert(person_id, person.clone());
+            let next_person_id = person_id.checked_add(1).ok_or(Error::<T>::PersonIdOverflow)?;
+            PersonId::<T>::put(next_person_id);
+            Self::deposit_event(Event::AddPerson(caller, person_id, person));
+            Ok(())
+        }
+        #[pallet::weight(10_000)]
+        pub fn update_person(origin: OriginFor<T>, person_id: u32, name: Vec<u8>, age: u8, address: Vec<u8>) -> DispatchResult {
+            let caller = ensure_signed(origin)?;
+            let new_person = Person {
+                name,
+                age,
+                address,
+            };
+            Persons::<T>::try_mutate_exists(person_id, |person| {
+                *person = Some(new_person.clone());
+                Self::deposit_event(Event::UpdatePerson(caller, person_id, new_person));
+                Ok(())
+            })
+        }
+    }
 }
